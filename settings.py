d3a1f706e0523c3296f02a03170b26f03695cbc3
Bilal Arshad Chaudhry <b.a.chaudhry@unswalumni.com>
2018-09-07 10:33:31 +0500

    Added bullet settings to settings.py
    These settings create dark gray bullets with a width of 3 pixels
    and height of 15 pixels. The bullets will travel slightly slower than
    the ship.

--- settings.py
+++ settings.py
@@ -3,10 +3,16 @@ class Settings():
 
     def __init__(self):
         """Initialize the game's settings."""
-        # Screen Settings
+        # Screen settings
         self.screen_width = 1200
         self.screen_height = 750
         self.bg_color = (255, 255, 255)
 
         # Ship settings.
         self.ship_speed_factor = 1.5
+
+        # Bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
