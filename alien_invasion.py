ef6fa40b58c9debe04ff3f85e8d4eda283a90a97
Bilal Arshad Chaudhry <b.a.chaudhry@unswalumni.com>
2018-09-10 14:48:51 +0500

    Created the first row of the alien fleet
    In order to figure out how many aliens fit in a single row, we
    calculate our horizontal space and kept an empty margin on either side
    of the screen. The formula used for this is:
    
    available_space_x = ai_settings.screen_width - (2 * alien_width)
    
    The spacing between the aliens was kept at one alien width. The space
    needed to display one alien is twice its width i.e. one width for the
    alien and one width for the empty space to its right. The resulting
    formula is:
    
    number_aliens_x = available_space_x / (2 * alien_width)
    
    Afterwards, aliens were added to an empty group called aliens in
    alien_invasion.py and a function was called in game_functions.py to
    create a fleet. Because we're no longer creating aliens directly in
    alien_invasion.py, we don't need to import the alien class.
    
    update_screen() was modified by calling draw() on the alien group.
    Finally, create_fleet() was placed in game_functions.py.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,6 @@
 import pygame
 from settings import Settings
 from ship import Ship
-from alien import Alien
 from pygame.sprite import Group
 import game_functions as gf
 
@@ -13,14 +12,13 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Make a ship and pass ai_settings as an argument.
+    # Make a ship, a group of bullets, and a group of aliens.
     ship = Ship(ai_settings, screen)
-
-    # Make a group to store bullets in.
     bullets = Group()
+    aliens = Group()
 
-    # Make an Alien craft.
-    alien = Alien(ai_settings, screen)
+    # Create a fleet of aliens.
+    gf.create_fleet(ai_settings, screen, aliens)
 
     # Start the main loop for the game.
     while True:
@@ -28,7 +26,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
 
--- settings.py
+++ settings.py
@@ -14,6 +14,6 @@ class Settings():
         # Bullet settings
         self.bullet_speed_factor = 1
         self.bullet_width = 3
-        self.bullet_height = 15
+        self.bullet_height = 10
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
