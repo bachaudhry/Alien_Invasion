d7794c10472c7ac24052b087328145163c07d446
Bilal Arshad Chaudhry <b.a.chaudhry@unswalumni.com>
2018-09-11 11:57:54 +0500

    Modified screen and bullet speed settings

--- settings.py
+++ settings.py
@@ -5,15 +5,15 @@ class Settings():
         """Initialize the game's settings."""
         # Screen settings
         self.screen_width = 1200
-        self.screen_height = 750
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
         # Ship settings.
         self.ship_speed_factor = 1.5
 
         # Bullet settings
-        self.bullet_speed_factor = 1
-        self.bullet_width = 3
+        self.bullet_speed_factor = 1.5
+        self.bullet_width = 3 
         self.bullet_height = 10
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
