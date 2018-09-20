4b862b0bc5290a61033a2f52129e8d88873fbbfa
Bilal Arshad Chaudhry <b.a.chaudhry@unswalumni.com>
2018-09-20 10:41:19 +0500

    Added leveling up feature
    Modified and grouped the speed settings in settings.py into static and
    dynamic settings. Added a speedup_scale to control the rate at which the
    game speeds up after every level. Added an increase_speed() function
    which multiplies the ship, alien and bullet speed factors to
    speedup_scale.
    
    Modified check_play_buttons() in game_functions.py to include the
    aforementioned functionality.

--- game_functions.py
+++ game_functions.py
@@ -54,6 +54,8 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     """Start a new game when the player clicks Play."""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # Apply difficulty scaling settings
+        ai_settings.initialize_dynamic_settings()
         # Hide the mouse cursor
         pygame.mouse.set_visible(False)
         # Reset the game statistics
@@ -105,7 +107,9 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
 
     # Generate new fleet if all aliens are destroyed.
     if len(aliens) == 0:
+        # Destroy existing bullets, speed up the game and create new fleet.
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 
--- settings.py
+++ settings.py
@@ -2,25 +2,43 @@ class Settings():
     """A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-        """Initialize the game's settings."""
+        """Initialize the game's Static settings."""
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
         # Ship settings.
-        self.ship_speed_factor = 1.5
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 10
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed_factor = 1
         self.fleet_drop_speed = 10
-        # Fleet direction of 1 represents right; -1 represents left.
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.2
+
+        self.initialize_dynamic_settings()
+
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 2
+        self.alien_speed_factor = 1.2
+
+        # Fleet direction of 1 represents right and -1 represents left.
         self.fleet_direction = 1
+
+
+    def increase_speed(self):
+        """Increase speed (difficulty) settings."""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+
