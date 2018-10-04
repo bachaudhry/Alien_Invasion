8ebc8e0a6801830e37b205bb1f2455b48abb3e4c
Bilal Arshad Chaudhry <b.a.chaudhry@unswalumni.com>
2018-10-04 19:00:18 +0500

    Corrected comments.

--- game_functions.py
+++ game_functions.py
@@ -111,7 +111,7 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, 
     # Remove any bullets which have collided with aliens.
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
-    # Adding scoring to bullet collisions
+    # Adding scoring (high scores as well) to bullet collisions
     if collisions:
         for aliens in collisions.values():
             stats.score += ai_settings.alien_points * len(aliens)
